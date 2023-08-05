8e899a66d4f6a44317b0292748b847980350b2c3
muhammadsyafiq1 <syafiqmuhammad655@gmail.com>
2023-08-05 18:44:25 +0700

    update

--- controllers/Product.js
+++ controllers/Product.js
@@ -78,6 +78,73 @@ export const createProduct = async (req, res) => {
     })
     
 }
+
+export const bulkCreateProduct = async (req, res) => {
+    if (req.files === null) return res.status(400).json({ msg: "Tidak ada berkas yang diunggah" });
+    
+    // Jika permintaan berisi array produk
+    if (Array.isArray(req.files.files) && req.files.files.length > 0) {
+        try {
+            const produk = req.body.produk;
+            const allowedType = ['.png','.jpg','.jpeg'];
+            const produkDibuat = [];
+
+            for (let i = 0; i < req.files.files.length; i++) {
+                const file = req.files.files[i];
+                const nama = produk[i].nama;
+                const harga = produk[i].harga;
+                const idPengguna = req.userId;
+                const ukuranBerkas = file.data.length;
+                const ekstensi = path.extname(file.name);
+                const namaBerkas = file.md5 + ekstensi;
+                const url = `${req.protocol}://${req.get("host")}/images/${namaBerkas}`;
+
+                if (!allowedType.includes(ekstensi.toLowerCase())) return res.status(422).json({ msg: "Jenis berkas tidak valid" });
+                if (ukuranBerkas > 5000000) return res.status(422).json({ msg: "Ukuran gambar harus kurang dari 5 MB" });
+
+                file.mv(`./public/images/${namaBerkas}`, async (err) => {
+                    if (err) return res.status(500).json({ msg: err.message });
+                    try {
+                        await Product.create({ nama: nama, gambar: namaBerkas, url: url, harga: harga, idPengguna: idPengguna });
+                        produkDibuat.push({ nama: nama, gambar: namaBerkas, url: url, harga: harga });
+                    } catch (error) {
+                        console.log(error.message);
+                    }
+                });
+            }
+
+            res.status(201).json({ msg: "Produk Berhasil Dibuat", produkDibuat });
+        } catch (error) {
+            console.log(error.message);
+            res.status(500).json({ msg: "Terjadi Kesalahan Server" });
+        }
+    } else {
+        // Jika permintaan hanya berisi satu produk
+        const { name, price } = req.body;
+        const file = req.files.file;
+        const idPengguna = req.userId;
+        const fileSize = file.data.length;
+        const ext = path.extname(file.name);
+        const fileName = file.md5 + ext;
+        const url = `${req.protocol}://${req.get("host")}/images/${fileName}`;
+        const allowedType = ['.png', '.jpg', '.jpeg'];
+
+        if (!allowedType.includes(ext.toLowerCase())) return res.status(422).json({ msg: "Jenis berkas tidak valid" });
+        if (fileSize > 5000000) return res.status(422).json({ msg: "Ukuran gambar harus kurang dari 5 MB" });
+
+        file.mv(`./public/images/${fileName}`, async (err) => {
+            if (err) return res.status(500).json({ msg: err.message });
+            try {
+                await Product.create({ name: name, image: fileName, url: url, price: price, userId: idPengguna });
+                res.status(201).json({ msg: "Produk Berhasil Dibuat" });
+            } catch (error) {
+                console.log(error.message);
+                res.status(500).json({ msg: "Terjadi Kesalahan Server" });
+            }
+        });
+    }
+};
+
  
 export const updateProduct = async (req, res) => {
     const product = await Product.findOne({
